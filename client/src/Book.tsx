4acd95af54e728bdf657c9fa661d43c4cb8a1bc1
Diego Balderrama <diegowrhasta@gmail.com>
2024-08-05 14:54:06 -0400

    Adds env variables for react application, cors for server application

--- client/src/Book.tsx
+++ client/src/Book.tsx
@@ -1,6 +1,8 @@
 import './Book.css'
 
 export function Book () {
+
+  console.log('server url', import.meta.env.VITE_REACT_APP_SERVER_URL)
   return (
     <>
       <div id='book-message'>Hello I'm a book</div>
